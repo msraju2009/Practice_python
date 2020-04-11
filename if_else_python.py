f02ccf43ab89d09716bbc92f8e85ecc44fc3885f
MadhuSudhanaRaju Chejerla <madhu.sud.chejerla@microfocus.com>
2020-04-11 14:27:54 +0530

    added if else program

---
+++ if_else_python.py
@@ -0,0 +1,9 @@
+
+username = "Madhu"
+password = raw_input("please input a password ")
+if username.lower() == "madhu" and password.lower() == "india":
+    print("You're are authenticated")
+else:
+    print("You're not authorised to login ")
+
+
