13e7de4d4e2861edb18b9b48b2f2f083d6409c51
MadhuSudhanaRaju Chejerla <madhu.sud.chejerla@microfocus.com>
2020-04-11 14:43:26 +0530

    Revert "checking revert functionality"
    This reverts commit eae5bdd5e7a5e6de8a8146b4c9e318a9dcb7bd43.

--- if_else_python.py
+++ if_else_python.py
@@ -3,7 +3,6 @@ username = "Madhu"
 password = raw_input("please input a password ")
 if username.lower() == "madhu" and password.lower() == "india":
     print("You're are authenticated")
-    print("hello")
 else:
     print("You're not authorised to login ")
 
